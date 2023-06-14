5a1ad216e9144db0fa0b0e689b2cab723c28753c
marisha831995 <132434037+marisha831995@users.noreply.github.com>
2023-06-14 15:43:29 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -11,8 +11,8 @@ SELECT track_name FROM track
 SELECT collection_name FROM collection_of_songs 
 	WHERE year_of_release BETWEEN 2018 and 2020;
 	
-SELECT executor_name FROM executor
-	WHERE executor_name NOT LIKE '%% %%';
+SELECT singer_name FROM singer
+	WHERE singer_name NOT LIKE '%% %%';
 	
 SELECT track_name FROM track
 	WHERE track_name LIKE '%%My%%';
