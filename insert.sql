2a88e425e11f879a3d7979d59962dfc7075d2203
marisha831995 <132434037+marisha831995@users.noreply.github.com>
2023-06-14 15:32:50 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -28,23 +28,17 @@ INSERT INTO track(track_name, duration, album_id)
 		('Deep and Dark', 219, 2), ('Blood Too Hot', 256, 2),
 		('Like Toy Soldiers', 297, 3), ('Just Lose It', 249, 3),
 		('Take Me To The Party', 187, 4), ('Want Some More', 201, 4),
-		('Back in Black', 256, 5), ('Rock and Rol Ain"t Noise Pollution', 183, 5),
-		('Burn it Down', 230, 6), ('Powerless', 225, 6),
-		('Supplies', 226, 7), ('Say Something', 279, 7),
-		('No Guidance', 261, 8), ('Dear God', 243, 8);
+		('Bring Me To Life', 237, 5), ('Going Under', 214, 5),
+		('Break', 236, 6), ('The Good Life', 253, 6),
+		('Carry Me', 266, 7), ('Live This Down', 216, 7),
+		('My Man's Gone Now', 240, 8), ('Summertime', 183, 8);
 
 INSERT INTO collection_of_songs(collection_name, year_of_release) 
-	values('Сборник1', 1970), ('Сборник2', 2014),
-		('Сборник3', 2018), ('Сборник4', 2019),
-		('Сборник5', 2019), ('Сборник6', 2020),
-		('Сборник7', 2020), ('Сборник8', 2020);
+	values('Сборник 1', 2015), ('Сборник 2', 2020),
+		('Сборник 3', 2017), ('Сборник 4', 2005),
+		('Сборник 5', 2012), ('Сборник 6', 2022),
+		('Сборник 7', 1934), ('Сборник 8', 2019);
 
 INSERT INTO track_collection(track_id, collection_of_songs_id) 
-	values(1,1), (3,1), (2,2), (4,2), (9,3), (11,3), (10,4), (12,4), 
-		(13,5), (15,5), (14,6), (16,6), (5,7), (7,7), (6,8), (8,8);
-
-INSERT INTO genre_executor(genre_id, executor_id) 
-	VALUES(3,7), (4,3);
-	
-INSERT INTO track(track_name, duration, album_id) 
-	values('Белый кит', 320, 3), ('Gnat', 225, 4);
+	values(1,2), (2,2), (3,4), (4,4), (5,3), (6,3), (7,6), (8,6), 
+		(9,5), (10,5), (11,1), (12,1), (13,8), (14,8), (15,7), (16,7);
